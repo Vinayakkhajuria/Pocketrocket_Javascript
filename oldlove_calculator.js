0cb80151c288d0b04a1e17dacd3b15c57dbe993e
Vinayakkhajuria <42890579+Vinayakkhajuria@users.noreply.github.com>
2020-04-04 14:57:10 +0530

    Add files via upload

---
+++ oldlove_calculator.js
@@ -0,0 +1,13 @@
+alert("WELCOME TO LOVE CALCULATOR");
+prompt("WHAT IS YOUR NAME?");
+prompt("WHAT IS YOUR LOVE'S NAME? ");
+
+var m = Math.random() * 100;
+m=Math.floor(m)+1;
+console.log(m);
+if(m>70){
+    alert("LOVE SCORE IS "+ " "+m+" "+" SO TRUE LOVE EXISTS");
+}
+else{
+alert("YOUR LOVE SCORE IS "+" "+m );
+}
